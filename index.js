25322ac1bea2b772af3a66c3e64d51e444f059f3
Dat Do <62579790+Thaehan@users.noreply.github.com>
2024-11-03 22:48:46 +0700

    Jitsi android setup

--- index.js
+++ index.js
@@ -8,13 +8,4 @@ import 'react-native-gesture-handler';
 import {name as appName} from './app.json';
 import App from './src/App';
 
-AppRegistry.registerHeadlessTask(
-  'RNCallKeepBackgroundMessage',
-  () => backgroundData => {
-    // Make your call here
-    console.log({backgroundData});
-    return Promise.resolve();
-  },
-);
-
 AppRegistry.registerComponent(appName, () => App);
